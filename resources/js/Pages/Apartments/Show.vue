0b1e1940a884b6869a1f4b74d34649df01ca60f4
GerryNdungu <gerryjerry36@gmail.com>
2023-01-10 14:24:49 +0300

    created dynamic units form with  btns

--- resources/js/Pages/Apartments/Show.vue
+++ resources/js/Pages/Apartments/Show.vue
@@ -85,21 +85,91 @@
                 </div>
                 <hr />
               </div>
-              <!-- TODO -  Add Create button for -->
+              <!-- TODO -  Implement Saving Units Array in the Backend -->
               <div class="overflow-hidden mt-3 bg-white shadow sm:rounded">
-                <button
-                  class="
-                    m-4
-                    p-4
-                    bg-green-400
-                    hover:bg-green-600
-                    text-sm
-                    rounded
-                  "
-                  type="button"
-                >
-                  Add Units
-                </button>
+                <form @submit.prevent="saveUnits()">
+                <fieldset class="ml-4">
+                  <legend>
+                    <span class="text-sm font-medium text-gray-700">Units</span>
+                    <button
+                      @click="addUnit()"
+                      class="
+                        m-4
+                        p-4
+                        bg-green-400
+                        hover:bg-green-600
+                        text-sm
+                        rounded
+                      "
+                      type="button"
+                    >
+                      Add Units
+                    </button>
+                  </legend>
+                  <div class="mt-1 mr-3">
+                    <div class="mb-2" v-for="(unit, index) in form.units" :key="index">
+                      <div class="flex space-x-2 items-center">
+                        <button @click="removeUnit(index)">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                          Trash
+                        </svg>
+                      </button>
+
+                        <div class="flex-auto">
+                          <input
+                            v-model="unit.unit_no"
+                            type="text"
+                            class="
+                              w-full
+                              focus:ring-yellow-500 focus:border-yellow-500
+                              block
+                              w-full
+                              sm:text-sm
+                              border-gray-300
+                            "
+                            :aria-label="`Unit {index+1}`"
+                            placeholder="Unit No"
+                          />
+                        </div>
+                        <div class="flex-auto ">
+                          <input
+                            v-model="unit.meter_no"
+                            type="number"
+                            class="
+                              w-full
+                              focus:ring-yellow-500 focus:border-yellow-500
+                              block
+                              w-full
+                              sm:text-sm
+                              border-gray-300
+                            "
+                            :aria-label="`Meter {index+1} No`"
+                            placeholder="Meter No"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex justify-end">
+
+                    <button type="submit" class="h-10 mb-4 mr-4 flex-end bg-yellow-500 sm:rounded inline-flex items-center justify-center px-4 border border-transparent shadow-sm text-sm font-semibold">
+                      Save Units
+                    </button>
+                  </div>
+                </fieldset>
+              </form>
               </div>
 
               <div class="overflow-hidden mt-3 bg-white shadow sm:rounded">
@@ -128,87 +198,89 @@
                 </div>
                 <!-- FIXME - Check these v-if/v-else statements -->
                 <div v-else class="border-t border-gray-200">
-                    <dl>
-                      <table class="min-w-full py-3 px-4 divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                          <tr>
-                            <th
-                              scope="col"
-                              class="
-                                px-6
-                                py-3
-                                text-left text-xs
-                                font-medium
-                                text-gray-500
-                                uppercase
-                                tracking-wider
-                              "
-                            >
-                              Unit
-                            </th>
-                            <th
-                              scope="col"
-                              class="
-                                px-6
-                                py-3
-                                text-left text-xs
-                                font-medium
-                                text-gray-500
-                                uppercase
-                                tracking-wider
-                              "
-                            >
-                              Meter No
-                            </th>
-                            <th
-                              scope="col"
-                              class="
-                                flex
-                                space-x-4
-                                px-6
-                                py-3
-                                text-left text-xs
-                                font-medium
-                                text-gray-500
-                                uppercase
-                                tracking-wider
-                              "
-                            ></th>
-                          </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                          <tr v-for="unit in units" :key="unit.id">
-                            <td class="px-6 py-4 whitespace-nowrap">
-                              {{ unit.unit_no }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                              {{ unit.meter_no }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                              <Link
-                              :href="route('units.show', unit.id )"
-                                class="text-green-600 hover:text-indigo-900"
-                                ><svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke-width="1.5"
-                                  stroke="currentColor"
-                                  class="w-6 h-6"
-                                >
-                                  <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                  />
-                                </svg>
-                              </Link>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </dl>
-                  </div>
+                  <dl>
+                    <table
+                      class="min-w-full py-3 px-4 divide-y divide-gray-200"
+                    >
+                      <thead class="bg-gray-50">
+                        <tr>
+                          <th
+                            scope="col"
+                            class="
+                              px-6
+                              py-3
+                              text-left text-xs
+                              font-medium
+                              text-gray-500
+                              uppercase
+                              tracking-wider
+                            "
+                          >
+                            Unit
+                          </th>
+                          <th
+                            scope="col"
+                            class="
+                              px-6
+                              py-3
+                              text-left text-xs
+                              font-medium
+                              text-gray-500
+                              uppercase
+                              tracking-wider
+                            "
+                          >
+                            Meter No
+                          </th>
+                          <th
+                            scope="col"
+                            class="
+                              flex
+                              space-x-4
+                              px-6
+                              py-3
+                              text-left text-xs
+                              font-medium
+                              text-gray-500
+                              uppercase
+                              tracking-wider
+                            "
+                          ></th>
+                        </tr>
+                      </thead>
+                      <tbody class="bg-white divide-y divide-gray-200">
+                        <tr v-for="unit in units" :key="unit.id">
+                          <td class="px-6 py-4 whitespace-nowrap">
+                            {{ unit.unit_no }}
+                          </td>
+                          <td class="px-6 py-4 whitespace-nowrap">
+                            {{ unit.meter_no }}
+                          </td>
+                          <td class="px-6 py-4 whitespace-nowrap">
+                            <Link
+                              :href="route('units.show', unit.id)"
+                              class="text-green-600 hover:text-indigo-900"
+                              ><svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-6 h-6"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                                />
+                              </svg>
+                            </Link>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </dl>
+                </div>
               </div>
             </div>
           </div>
@@ -225,16 +297,39 @@ import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
 import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 import { HomeIcon } from "@heroicons/vue/20/solid";
 import { ref } from "vue";
+import { Inertia } from "@inertiajs/inertia";
 
 const props = defineProps({
   apartment: Object,
   units: Object,
   unitsCount: Object,
+  errors:Object
 });
 
-const storeAgreement = () => {
-  processing.value = true;
-  form.post("/agreements");
-};
+const form = useForm({
+  units:[]
+}) 
+
+const addUnit = ()=>{
+  form.units.push({
+    unit_no:"",
+    meter_no:0,
+    
+  })
+}
+
+const removeUnit = (index)=>{
+  form.units.splice(index,1)
+}
+
+const saveUnits = ()=>{
+  form.post('/units') 
+  // Inertia.post('/units',{ 
+  //   _method:'post',
+  //   unit_no:form.unit_no,
+  //   meter_no:form.meter_no,
+  // })
+}
+
 </script>
   
